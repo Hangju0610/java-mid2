e24663dc3139ba58f35104dbc3b9f9083e1379d4
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2025-02-11 22:17:06 +0900

    add:MyList

---
+++ src/collection/list/MyArrayList.java
@@ -0,0 +1,109 @@
+package collection.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E>{
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayList() {
+        /*
+        제네릭은 런타임에 이레이저에 의해 타입정보가 사라지기 때문에 타입 정보가 필요한 생성자에 사용할 수 없다는 단점 존재.
+        따라서 Object로 넣을 수 밖에 없다.
+        */
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    // 코드 추가
+    @Override
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        shiftRightFrom(index);
+    }
+
+    // 코드 추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+
+        }
+    }
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        // 기존 배열을 참조하여 새로운 크기의 배열을 만들어낸다.
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    // 타입 체크 무시하도록 설정하는 애너테이션
+    @Override
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    // 코드 추가
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    // 코드 추가, 요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1 ; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
+    @Override
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size=" +
+                size + ", capacity=" + elementData.length;
+    }
+}
