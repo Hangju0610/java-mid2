9d9ec33dda8caa08b802da0cf57710fe4749ad09
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2025-03-25 23:22:05 +0900

    add:iterator, iterable 2

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -12,5 +12,11 @@ public class MyArrayMain {
             Integer value = iterator.next();
             System.out.println("value = " + value);
         }
+
+        // 위 아래 두 코드는 같은 코드이다. 아래 코드 for-each로 사용시 컴파일 시점에 while문과 같이 코드를 변경한다.
+        System.out.println("for-each 사용");
+        for (int value : myArray) {
+            System.out.println("value = " + value);
+        }
     }
 }
